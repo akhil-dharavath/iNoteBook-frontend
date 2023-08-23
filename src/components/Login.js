26f159a47f448b6fb4db4f3e2a4cc71a40682024
akhil <86508192+akhildgithub@users.noreply.github.com>
2023-08-23 14:48:45 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,10 @@ const Login = (props) => {
   }, [])
   const handleSubmit = async (e) => {
     e.preventDefault()
-    const response = await fetch(`http://localhost:1000/api/auth/login`, {
+    const response = await fetch(
+      // `http://localhost:1000/api/auth/login`
+      `https://inotebook-backend-hdoh.onrender.com/api/auth/login`
+      , {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
